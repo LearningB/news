ad2a156b2ab8ac7a264254201b9f430c503a868f
Bjn <siknucha@gmail.com>
2019-05-06 15:48:33 -0700

    added first newspaper

---
+++ kantipur.py
@@ -0,0 +1,22 @@
+import newspaper
+import pandas
+
+kantipur_news = newspaper.build("http://kathmandupost.ekantipur.com/", memoize_articles=False)
+kantipur_news_data = []
+
+for article in kantipur_news.articles:
+    article_data = {}
+    article.download()
+    article.parse()
+    article.nlp()
+    article_data["title"] = article.title
+    article_data["url"] = article.url
+    article_data["summary"] = article.summary
+    article_data["meta_keywords"] = article.meta_keywords
+    article_data["date"] = article.publish_date
+    kantipur_news_data.append(article_data)
+
+
+
+kantipur_data = pandas.DataFrame(kantipur_news_data)
+kantipur_data.to_csv("kantipur_news")
