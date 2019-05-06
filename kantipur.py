96c042dd40d3cc0e67622bb72482c31d0fb4cec1
Bjn <siknucha@gmail.com>
2019-05-06 15:58:42 -0700

    targetting only kathmandu po st

--- kantipur.py
+++ kantipur.py
@@ -6,15 +6,16 @@ kantipur_news_data = []
 
 for article in kantipur_news.articles:
     article_data = {}
-    article.download()
-    article.parse()
-    article.nlp()
-    article_data["title"] = article.title
-    article_data["url"] = article.url
-    article_data["summary"] = article.summary
-    article_data["meta_keywords"] = article.meta_keywords
-    article_data["date"] = article.publish_date
-    kantipur_news_data.append(article_data)
+    if "kathmandupost" in article.url:
+        article.download()
+        article.parse()
+        article.nlp()
+        article_data["title"] = article.title
+        article_data["url"] = article.url
+        article_data["summary"] = article.summary
+        article_data["meta_keywords"] = article.meta_keywords
+        article_data["date"] = article.publish_date
+        kantipur_news_data.append(article_data)
 
 
 
